cf709a21257c86a97c800c9580f8d6d155a821c3
vansmoe <vansmoe@hotmail.com>
2020-09-18 21:12:20 +0200

    New realese plannig

--- js/covid-banner.js
+++
@@ -1,8 +0,0 @@
-let covidDiv = document.querySelector(".ftco-covid19")
-
-function fixedCovidDiv(){
-  covidDiv.style.position="unset";
-}
-document.onscroll = () => {
-  if(scrollY >  covidDiv.scrollHeight) fixedCovidDiv()
-};
