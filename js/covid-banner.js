39b4c514e73e9e3aa1cbad9be6ef25cbf62e16a1
vansmoe <vansmoe@hotmail.com>
2020-09-25 22:01:46 +0200

    Covid banner added

---
+++ js/covid-banner.js
@@ -0,0 +1,9 @@
+const covidBanner = document.querySelector(".covidBanner");
+
+function fixedCovidBanner() {
+    covidBanner.style.position = "unset";
+};
+
+document.onscroll = () => {
+    if(scrollY >  covidBanner.scrollHeight) fixedCovidBanner()
+}
