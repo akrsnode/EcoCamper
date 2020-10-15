41296e87012aed7164cdff7cbc09d6b8e8e7961a
vansmoe <vansmoe@hotmail.com>
2020-10-15 18:17:54 +0200

    Menu: fixed menu after scrolling

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Changing navbar on scrolling
+const navbarDiv = document.querySelector('.navbar');
+const menuLinks = document.querySelectorAll('.menu__nav__link');
+let fixed = false;
+
+const changeNavbar = function() {
+  if(fixed == false && scrollY > 120) {
+    navbarDiv.classList.add('navbar--extend-scrolled');
+    menuLinks.forEach(elem => elem.classList.add('menu__nav__link--dark'));
+  }
+  if(fixed == false && scrollY > 500) {
+    fixed = true;
+    navbarDiv.classList.add('navbar--extend-fixed');
+  };
+  if(fixed == true && scrollY < 500) {
+    fixed = false;
+    navbarDiv.classList.remove('navbar--extend-fixed');
+    navbarDiv.classList.remove('navbar--extend-scrolled');
+    menuLinks.forEach(elem => elem.classList.remove('menu__nav__link--dark'));
+  }
+};
+
+//Event handling
+
+window.addEventListener("scroll", changeNavbar);
