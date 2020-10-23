24c31a6c88c20dfb1660ca8f92d1e6751f580503
vansmoe <vansmoe@hotmail.com>
2020-10-23 20:58:45 +0200

    Home: added img slider

--- js/slider.js
+++ js/slider.js
@@ -3,15 +3,54 @@ let stageTime = 5000;
 
 
 //App
-let sliderItems = document.querySelectorAll('.product__slider__stage__item');
-console.log(sliderItems);
-let curIndex = 0;
 
-const changeSlide = () => {
-    if(curIndex >= sliderItems.length - 1) curIndex = 0
-    sliderItems[curIndex].classList.toggle('nodis');
-    if(curIndex >= sliderItems.length - 1) curIndex = 0
-    sliderItems[curIndex + 1].classList.toggle('nodis');
-    curIndex++;
+let sliderItems = document.querySelectorAll('.product__slider__stage__item');
+let slideIndex = 0;
+let stageQuantity = 1;
+
+const changeSlide = (ind) => {
+	for(let i = 0; i < stageQuantity; i++) {
+		ind += i;
+		if(ind >= sliderItems.length) ind = 0;
+		sliderItems[ind].classList.remove('nodis');
+	}
+};
+
+const clearSlide = () => {
+	sliderItems.forEach(elem => elem.classList.add('nodis'));
+};
+
+const runSlider = () => {
+	clearSlide();
+	changeSlide(slideIndex++);
+};
+
+const setStageQuantity = function() {
+	clearSlide();
+	if(this.innerWidth >= 1200) stageQuantity = 3;
+	else if(this.innerWidth >= 768) stageQuantity = 2;
+	else stageQuantity  = 1;
+	changeSlide(slideIndex);
 }
-setInterval(changeSlide, stageTime);
+setStageQuantity();
+
+slideInterval = setInterval(runSlider, stageTime);
+
+//Add nav buttons
+
+const addButtons = () => {
+	let sliderBtn = document.getElementById('slider-nav-btn');
+	for(let i = 1; i < sliderItems.length; i++) {
+		document.getElementById('slider-nav').appendChild(sliderBtn.cloneNode(true))
+	}
+}
+addButtons();
+
+document.querySelectorAll('#slider-nav-btn').forEach((item, index) => item.addEventListener('click', function() {
+	clearInterval(slideInterval);
+	clearSlide();
+	changeSlide(index);
+	slideInterval = setInterval(runSlider, stageTime);
+}))
+
+window.addEventListener('resize', setStageQuantity);
