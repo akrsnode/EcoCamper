f42a1eb0de48fb526b84d16f1ece6b392b80272f
vansmoe <vansmoe@hotmail.com>
2020-10-24 11:27:33 +0200

    Functions added to slider

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,10 @@ let stageTime = 5000;
 
 //App
 
-let sliderItems = document.querySelectorAll('.product__slider__stage__item');
+let sliderItems = document.querySelectorAll('#slider-item');
+let stackable = document.getElementById('slider').getAttribute('data-stackable');
+let navigate = document.getElementById('slider').getAttribute('data-nav');
+
 let slideIndex = 0;
 let stageQuantity = 1;
 
@@ -25,15 +28,6 @@ const runSlider = () => {
 	changeSlide(slideIndex++);
 };
 
-const setStageQuantity = function() {
-	clearSlide();
-	if(this.innerWidth >= 1200) stageQuantity = 3;
-	else if(this.innerWidth >= 768) stageQuantity = 2;
-	else stageQuantity  = 1;
-	changeSlide(slideIndex);
-}
-setStageQuantity();
-
 slideInterval = setInterval(runSlider, stageTime);
 
 //Add nav buttons
@@ -44,13 +38,28 @@ const addButtons = () => {
 		document.getElementById('slider-nav').appendChild(sliderBtn.cloneNode(true))
 	}
 }
-addButtons();
 
-document.querySelectorAll('#slider-nav-btn').forEach((item, index) => item.addEventListener('click', function() {
-	clearInterval(slideInterval);
+//Stage quantity
+const setStageQuantity = function() {
 	clearSlide();
-	changeSlide(index);
-	slideInterval = setInterval(runSlider, stageTime);
-}))
+	if(this.innerWidth >= 1200) stageQuantity = 3;
+	else if(this.innerWidth >= 768) stageQuantity = 2;
+	else stageQuantity  = 1;
+	changeSlide(slideIndex);
+}
 
-window.addEventListener('resize', setStageQuantity);
+//Render
+if(stackable == "true") {
+	setStageQuantity();
+	window.addEventListener('resize', setStageQuantity);
+};
+
+if(navigate == "true") {
+	addButtons();
+	document.querySelectorAll('#slider-nav-btn').forEach((item, index) => item.addEventListener('click', function() {
+		clearInterval(slideInterval);
+		clearSlide();
+		changeSlide(index);
+		slideInterval = setInterval(runSlider, stageTime);
+	}))	
+}
